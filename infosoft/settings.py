45fbb94abbb4b5962224e0b5d2902a3ade9d7049
Jimmy Alcala <jimmyacala@gmail.com>
2013-07-12 04:04:41 +0000

    Agregado trampa para que funcione los static

--- infosoft/settings.py
+++ infosoft/settings.py
@@ -44,8 +44,8 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
 
+STATIC_ROOT = '/root/Sites/infosoft/infosoft/static/'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
